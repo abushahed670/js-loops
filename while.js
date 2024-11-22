43e03dd4c4de99bd754376c2960e49a01f16e0e4
abushahed670 <abu.shahed670@gmail.com>
2024-11-22 22:35:24 +0600

    while loop added

---
+++ while.js
@@ -0,0 +1,16 @@
+const number =7;
+// if(number < 10){
+//     console.log('got small number');
+// }
+// else{
+//     console.log('go back to check');
+// }
+
+//..........while loop..........
+
+let num = 3;
+while(num < 7){
+    console.log('go back', num)
+    // num = num + 1;
+    num++;
+}
