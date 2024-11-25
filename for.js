c1f5b7364a416bf1c5a38abd2bcf55f386750aab
abushahed670 <abu.shahed670@gmail.com>
2024-11-25 22:38:10 +0600

    for loop added

---
+++ for.js
@@ -0,0 +1,10 @@
+// the structure of for loop
+// for(first part; second part; third part ){
+//     loop statement;
+// }
+
+ /**for(loop variable declaration; loop condition; loop variable change ) */
+
+for(let  num = 0; num < 5; num++){
+    console.log(num);
+}
