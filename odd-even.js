6a1e1af41cd006d7762d24601cf4f1041510c9b9
abushahed670 <abu.shahed670@gmail.com>
2024-11-25 22:46:52 +0600

    odd and even number finding added

---
+++ odd-even.js
@@ -0,0 +1,15 @@
+// for(let i =0; i <=10; i++){
+//     console.log(i);
+// }
+
+// even number 
+
+for(let i=0; i <=20; i=i+2){
+    console.log(i);
+}
+
+// odd number 
+
+for(let i=1; i<=20; i=i+2){
+    console.log(i);
+}
