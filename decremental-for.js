dfa8a6ea77081130f46ce04e5b70a8c07cc57ad7
abushahed670 <abu.shahed670@gmail.com>
2024-11-25 23:53:18 +0600

    decremental added

---
+++ decremental-for.js
@@ -0,0 +1,9 @@
+// incremental loop which increase 1 at a stage 
+// for(let i =0; i <= 10; i++){
+//     console.log(i);
+// }
+
+// decremental loop which decreases 1 at one operational loop 
+for(i = 10; i >= 0; i--){
+    console.log(i)
+}
